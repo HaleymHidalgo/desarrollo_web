663d5bc46807f63c38255a9127287932b36cb471
unknown <SSDD@PNAWSA06LC1028.duoc.acad>
2024-04-30 12:11:59 -0400

    Clase Async

---
+++ U2-04/assets/js/script.js
@@ -0,0 +1,13 @@
+const addDigimones = async () => {
+    return (1+1);
+};
+
+/*
+Lista de Conceptos a Estudiar
+
+    - Funciones Asincronas
+    - Promesas
+    - Usar el local storage
+    - 1 Api / 2 LocalStorage
+
+*/
